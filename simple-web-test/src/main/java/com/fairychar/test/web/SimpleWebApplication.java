795fe0ae108df36ee2cb5bc49fdf70ad3675894d
chiyo <1261975105@qq.com>
2020-12-10 16:03:00 +0800

    1

---
+++ simple-web-test/src/main/java/com/fairychar/test/web/SimpleWebApplication.java
@@ -0,0 +1,17 @@
+package com.fairychar.test.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Datetime: 2020/12/10 15:32 <br>
+ *
+ * @author chiyo <br>
+ * @since 1.0
+ */
+@SpringBootApplication
+public class SimpleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplication.class, args);
+    }
+}
